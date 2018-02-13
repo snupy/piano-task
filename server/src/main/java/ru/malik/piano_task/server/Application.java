0489b1002eba92bbffd41df77cad459b09640f0e
malik2000 <shmalik@yandex.ru>
2018-02-13 23:28:28 +0300

    test-commit-41

--- server/src/main/java/ru/malik/piano_task/server/Application.java
+++ server/src/main/java/ru/malik/piano_task/server/Application.java
@@ -25,7 +25,7 @@ public class Application {
     public class Config {
         @Bean
         public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
-            System.out.print("MALIK2000-test-13");
+            System.out.print("MALIK2000-test-21");
             return new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
         }
 
