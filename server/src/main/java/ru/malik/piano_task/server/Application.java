5beca9365041edc89f944a80e8516c38a5953e0d
snupy <gshmalik@gmail.com>
2018-02-13 22:49:03 +0300

    TEST

--- server/src/main/java/ru/malik/piano_task/server/Application.java
+++ server/src/main/java/ru/malik/piano_task/server/Application.java
@@ -25,7 +25,7 @@ public class Application {
     public class Config {
         @Bean
         public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
-            System.out.print("SSS-100500-TE");
+            System.out.print("TEST-02");
             return new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
         }
 
