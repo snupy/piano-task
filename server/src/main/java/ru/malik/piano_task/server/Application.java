dff571c9874291b008f047543be94fa8999f4421
snupy <gshmalik@gmail.com>
2018-02-13 22:47:35 +0300

    TEST

--- server/src/main/java/ru/malik/piano_task/server/Application.java
+++ server/src/main/java/ru/malik/piano_task/server/Application.java
@@ -25,7 +25,7 @@ public class Application {
     public class Config {
         @Bean
         public HttpComponentsClientHttpRequestFactory httpRequestFactory() {
-            System.out.print("TEST-03");
+            System.out.print("TEST-03-01");
             return new HttpComponentsClientHttpRequestFactory(HttpClientBuilder.create().build());
         }
 
